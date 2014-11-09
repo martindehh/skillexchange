7c927464e7e714819b8d2f046a0dbc183c3ab05e
martin.meier <martin.meier@protonet.info>
2014-11-09 14:54:07 +0100

    Completed create_ functions for tze nodes

--- view1/nodes-services.js
+++ view1/nodes-services.js
@@ -7,14 +7,44 @@ angular.module('nodes-services', ['ngResource']).factory('nodesService', [ '$res
         ],
 
 	create_employer: function(name, location, sector, username) {
-	        var node = $resource('//localhost:7474/db/data/node/');
-	        	var employer = node.get();
-			employer.name = name;
-			employer.location = location;
-			employer.sector = sector;
-			employer.username = username;
-			employer.save();
-	        },
+		var node = $resource('//localhost:7474/db/data/node/');
+	        var employer = node.get();
+		employer.name = name;
+		employer.location = location;
+		employer.sector = sector;
+		employer.username = username;
+		employer.created = new Date().getTime();
+		employer.save();
+	},
+	create_position: function(title, payment, timeframe) {
+		var node = $resource('//localhost:7474/db/data/node/');
+		var position = node.get();
+		position.title = title;
+		position.payment = payment;
+		position.timeframe = timeframe;
+		position.save();
+	},
+	create_skill: function(name) {
+		var node = $resource('//localhost:7474/db/data/node/');
+		var skill = node.get();
+		skill.name = name;
+		skill.save();
+	},
+	create_profile: function(title, payment, timeframe) {
+		var node = $resource('//localhost:7474/db/data/node/');
+		var profile = node.get();
+		profile.title = title;
+		profile.payment = payment;
+		profile.timeframe = timeframe;
+		profile.save();
+	},
+	create_person: function(username) {
+		var node = $resource('//localhost:7474/db/data/node/');
+		var person = node.get();
+		person.username = username;
+		person.created = new Date().getTime();
+		pseron.save();
+	},
     };
 
     return service;
