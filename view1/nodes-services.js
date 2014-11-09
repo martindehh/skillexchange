546ff8af5606d91a956d318076faeb13421d62f5
martin.meier <martin.meier@protonet.info>
2014-11-09 15:39:50 +0100

    Added connection between position and skill

--- view1/nodes-services.js
+++ view1/nodes-services.js
@@ -49,13 +49,26 @@ angular.module('nodes-services', ['ngResource']).factory('nodesService', [ '$res
 		var employer_node = $resource('//localhost:7474/db/data/node/:employer_id', {employer_id:'@employer_id'});
 		var position_node = $resource('//localhost:7474/db/data/node/:position_id', {position_id:'@position_id'});
 		var node = $resource('//localhost:7474/db/data/node/');
-		var employer = employer_node.get();
-		var position = position_node.get();
+		var employer = employer_node.$get();
+		var position = position_node.$get();
 		var connection = node.get();
 		connection.start = employer.id;
 		connection.end = position.id;
 		connection.type = 'HAS';
 		connection.$save();	
+	},
+	connect_position_musthave_skill: function(position_id, skill_id, weight) {
+                var position_node = $resource('//localhost:7474/db/data/node/:position_id', {position_id:'@position_id'});
+		var skill_node = $resource('//localhost:7474/db/data/node/:skill_id', {skill_id:'@skill_id'});
+                var node = $resource('//localhost:7474/db/data/node/');
+                var position = position_node.$get();
+		var skill = skill_node.$get();
+                var connection = node.get();
+                connection.start = position.id;
+                connection.end = skill.id;
+                connection.type = 'MUST HAVE';
+		connection.weight = [{"data": { "weight": weight }}];
+                connection.$save();
 	},
     };
 
